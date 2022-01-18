d3ac6a1ac0d9b1b7cbd3bcf696a7be232c5c7958
meghnathomas <meghnathomas@utexas.edu>
2022-01-18 16:30:22 -0600

    edited long_description content type in setup.py

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='magnets',
     name='magnets',
